402853d3422a6e1a9accd789e57eb8e72d196831
Farhan Shahid <123633657+farhan7479@users.noreply.github.com>
2024-06-10 14:10:49 +0530

    redux cinfigured

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 
 import React from 'react'
 import Main from './Main'
-
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 
 const App = () => {
   return (
-    <Main />
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Main />
+    </PersistGate>
+  </Provider>
+
   )
 }
 
---
+++ redux/store.js
@@ -0,0 +1,20 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import userReducer from './users/UserSlice';
+
+const rootReducer = combineReducers({ user: userReducer });
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+export const persistor = persistStore(store);
